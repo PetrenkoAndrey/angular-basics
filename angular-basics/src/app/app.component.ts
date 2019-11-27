3975dec09187fac90f61105d5ea52b082296a8a4
Petrenko Andrey <dd270386pai@gmail.com>
2019-11-27 16:32:39 +0200

    new project

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dynamic title'
-  number = 42
-  arr = [1,2,3]
-  obj = {a:3, b:{c:4}}
+
 }
