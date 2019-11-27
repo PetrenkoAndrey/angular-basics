51bff454b2466db4ed115595c66ae7d250cf6977
Petrenko Andrey <dd270386pai@gmail.com>
2019-11-27 10:31:11 +0200

    first

---
+++ angular-basics/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Dynamic title'
+  number = 42
+  arr = [1,2,3]
+  obj = {a:3, b:{c:4}}
+}
---
+++ angular-basics/src/app/post3/post3.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post3',
+  template: `
+    <p>
+      post3 works!
+    </p>
+  `,
+  styleUrls: ['./post3.component.scss']
+})
+export class Post3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
